44a86757cc294c570e7a7606be278a8d7913e37e
rmontroy <rob.montroy@vai.org>
2022-03-22 19:59:08 -0400

    add gtc short-circuit

--- gencall/app.py
+++ gencall/app.py
@@ -4,6 +4,7 @@ import os
 import json
 from urllib.parse import unquote_plus
 import boto3
+import botocore
 from datetime import datetime, timedelta
 import subprocess
 
@@ -34,6 +35,15 @@ def lambda_handler(event, _context):
     sample = key[:-9]
     logger.info('idat prefix = %s', sample)
     event_time = datetime.fromisoformat(event['Records'][0]['eventTime'][:-1])
+
+    # skip processing the new idat if we also have a (fresh) gtc
+    try:
+        s3.head_object(Bucket=bucket, Key=f'{sample}.gtc', IfModifiedSince=event_time-timedelta(minutes=1))
+        logger.info('%s.gtc already exists. Skipping.', sample)
+        return
+    except botocore.exceptions.ClientError:
+        logger.info('%s.gtc missing or not current. Generating...', sample)
+        pass
 
     s3.download_file(bucket, f'{sample}_Red.idat', f'/tmp/{sample}_Red.idat')
     # wait up to 5 seconds for Grn file, but make sure it's not an old version
