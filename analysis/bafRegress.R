ae7ef5791f447cae4fb982644897ac7d86a8e6c3
Rob Montroy <rob.montroy@gmail.com>
2022-04-02 01:20:58 +0000

    process .gtc files directly

--- analysis/bafRegress.R
+++ analysis/bafRegress.R
@@ -1,32 +1,42 @@
+library(reticulate)
+library(glue)
+
 testsamplecontamination <- function(baf, abgeno, maf, subset=NULL, ...) {
-	stopifnot(all(length(baf)==length(maf), length(baf)==length(abgeno)))
-	maf[maf>.5] <- 1-maf[maf>.5]
-	amaf <- ifelse(abgeno==2, -maf, maf)
-	callrate <- 1-mean(abgeno==3)
-	genocat <- factor(abgeno, levels=c(0,2))
-	subs <- abgeno==2 | abgeno==0
-	if (!is.null(subset)) subs<-subs & subset
-	if (all(table(genocat[subs])>0)) {
-		fit <- lm(baf~amaf+genocat, subset=subs, ...)
-	} else {
-		fit <- lm(baf~amaf, subset=subs, ...)
-	}
-	a <- c(coefficients(summary(fit))[2,], callrate, nrow(fit$model))
-	names(a) <- c("estimate","stderr","tval","pval","callrate", "Nhom")
-	return(a)
+    stopifnot(all(length(baf)==length(maf), length(baf)==length(abgeno)))
+    maf[maf>.5] <- 1-maf[maf>.5]
+    amaf <- ifelse(abgeno=="BB", -maf, maf)
+    callrate <- 1-mean(abgeno=="NC")
+    genocat <- factor(abgeno, levels=c(0,2))
+    subs <- abgeno=="BB" | abgeno=="AA"
+    if (all(table(genocat[subs])>0)) {
+        fit <- lm(baf~amaf+genocat, subset=subs, ...)
+    } else {
+        fit <- lm(baf~amaf, subset=subs, ...)
+    }
+    a <- c(coefficients(summary(fit))[2,], callrate, nrow(fit$model))
+    names(a) <- c("estimate","stderr","tval","pval","callrate", "Nhom")
+    return(a)
 }
 
-testsamples <- function(samples, maf, getgtc) {
-    for (i in seq_along(samples$sample)) {    
-        #dd <- do.call(getrawdata, c(list(file, sample=samples$sample[i]), options))
-        dd <- getgtc(samples$sample[i])
-        reg <- testsamplecontamination(dd$BAF, dd$ABGENO, maf)
+bead_array_files <- import('IlluminaBeadArrayFiles')
+bucket <- 'pbc-iscan-qcarrays'
+s3 <- paws::s3()
+
+testsamples <- function(samples, maf) {
+    for (i in seq_along(samples$Sample_ID)) {
+        filename <- glue('{samples$Barcode[i]}_{samples$Position[i]}.gtc')
+        s3$download_file(bucket, filename, filename)
+
+        gtc <- bead_array_files$GenotypeCalls(filename)
+        baf <- gtc$get_ballele_freqs()
+        abgeno <- unlist(sapply(gtc$get_genotypes(), function(x) bead_array_files$code2genotype[x+1]))
+        reg <- testsamplecontamination(baf, abgeno, maf)
         if (i==1) {
-            allcoef <- matrix(0, ncol=length(reg), nrow=length(samples$sample))
+            allcoef <- matrix(0, ncol=length(reg), nrow=length(samples$Sample_ID))
             colnames(allcoef)<-names(reg)
         }
         allcoef[i,] <- reg
     }
-    dd <- data.frame(sample=samples$sample, allcoef)
+    dd <- data.frame(Sample_ID=samples$Sample_ID, allcoef)
     return(dd)
 }
