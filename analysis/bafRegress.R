5d9acf72743ac8b492c0349ef8997730acedbb28
Rob Montroy <rob.montroy@gmail.com>
2022-04-04 03:12:24 +0000

    MAF calc; refactor bafRegress

--- analysis/bafRegress.R
+++ analysis/bafRegress.R
@@ -1,6 +1,3 @@
-library(reticulate)
-library(glue)
-
 testsamplecontamination <- function(baf, abgeno, maf, subset=NULL, ...) {
     stopifnot(all(length(baf)==length(maf), length(baf)==length(abgeno)))
     maf[maf>.5] <- 1-maf[maf>.5]
@@ -16,27 +13,4 @@ testsamplecontamination <- function(baf, abgeno, maf, subset=NULL, ...) {
     a <- c(coefficients(summary(fit))[2,], callrate, nrow(fit$model))
     names(a) <- c("estimate","stderr","tval","pval","callrate", "Nhom")
     return(a)
-}
-
-bead_array_files <- import('IlluminaBeadArrayFiles')
-bucket <- 'pbc-iscan-qcarrays'
-s3 <- paws::s3()
-
-testsamples <- function(samples, maf) {
-    for (i in seq_along(samples$Sample_ID)) {
-        filename <- glue('{samples$Barcode[i]}_{samples$Position[i]}.gtc')
-        s3$download_file(bucket, filename, filename)
-
-        gtc <- bead_array_files$GenotypeCalls(filename)
-        baf <- gtc$get_ballele_freqs()
-        abgeno <- unlist(sapply(gtc$get_genotypes(), function(x) bead_array_files$code2genotype[x+1]))
-        reg <- testsamplecontamination(baf, abgeno, maf)
-        if (i==1) {
-            allcoef <- matrix(0, ncol=length(reg), nrow=length(samples$Sample_ID))
-            colnames(allcoef)<-names(reg)
-        }
-        allcoef[i,] <- reg
-    }
-    dd <- data.frame(Sample_ID=samples$Sample_ID, allcoef)
-    return(dd)
 }
