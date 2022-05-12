bde842e8dab22879a394eff59c0541693f1c8e31
Rob Montroy <rob.montroy@gmail.com>
2022-05-12 02:04:42 +0000

    refactor nb to read from parquet files

--- analysis/popmaf.R
+++ analysis/popmaf.R
@@ -1,80 +1,59 @@
 library(data.table, warn.conflicts = FALSE)
 
+# Fastest way to get sample counts
+num_rows <- function(JIRA) {
+    ParquetFileReader$create(glue('s3://pbc-qcarray-sample-info/{JIRA}/sample_info.parquet'))$num_rows
+}
+
 # Calculate the population frequencies by averaging across
 # all projects/batches in the study/cohort
-generate.popmaf <- function(batches, manifest) {
-    batches %>% map(~ .$nsamples) %>% unlist %>% as.integer %>% sum -> npop
-    batches %>% map(~ get.batch.freq(.,manifest)) %>%
-        bind_rows(.id="JIRA") %>%
+calc_popmaf <- function(batches, bucket) {
+    npop <- batches %>% summarize(npop = sum(nsamples)) %>% unlist
+    popmaf <- batches %>%
+        group_by(JIRA) %>%
+        summarize(get_batch_freq(JIRA,bucket), .groups="keep") %>%
         group_by(snp) %>%
-        summarise(maf=sum(maf) / npop) -> popmaf
+        summarize(maf=sum(maf) / npop)
     return(popmaf)
 }
 
 # Get the batch frequencies for a given project/batch
-get.batch.freq <- function(batch, manifest) {
+get_batch_freq <- function(jira, bucket) {
     # See if the MAFs for this project/batch are in the cache
-    s3 <- paws::s3()
-    key <- paste0(batch$JIRA,".freq.gz")
-    filename <- paste0("/tmp/",key)
-    if (file.exists(filename)) {
-        print(glue::glue("File {filename} exists!"))
-        fread(filename) %>% 
-            as_tibble() %>% 
-            mutate(snp=manifest$names) %>%
-            relocate(snp) -> batch.freq
-        return(batch.freq)
-    }
-    result <- try(s3$download_file(bucket, key, filename), silent=TRUE)
-    if (!inherits(result, "try-error")) {
-        print(glue::glue("Key {key} exists!"))
-        fread(filename) %>% 
-            as_tibble() %>% 
-            mutate(snp=manifest$names) %>%
-            relocate(snp) -> batch.freq
-        return(batch.freq)
-    }
-    if(!grepl("NoSuchKey", result)) {
-        stop(result)
-    }
-    
-    # else calculate them
-    #json <- httr::content(httr::GET("http://{endpoint}/sampleinfo?batchid={batch$JIRA}"))
-    json <-
-    '[
-        {"Sample_ID" : "CPT0nnnnn_0001", "Barcode" : "203323200003", "Position" : "R11C02"},
-        {"Sample_ID" : "CPT0nnnnn_0002", "Barcode" : "203323200003", "Position" : "R11C02"}
-    ]'
-    sample.info <- jsonlite::fromJSON(json, simplifyDataFrame=FALSE)
-    names(sample.info) <- map(sample.info, ~ .$Sample_ID)
-    marker.freq <- function(AA, AB, BB) (AB+2*BB)/(2*(AA+AB+BB))
-    sample.info %>% 
-        map(~ data.frame(snp=manifest$names,abgeno=get.allele.data(.)) %>%
-        mutate(snp=manifest$names) %>%
-        relocate(snp) %>%
-        bind_rows(.id="Sample_ID") %>%
-        group_by(snp, abgeno) %>%
-        summarise(n=n(), .groups="drop_last") %>%
-        spread(abgeno, n) %>%
-        replace(is.na(.), 0) %>%
-        summarise(maf=marker.freq(AA,AB,BB)) -> batch.freq
+    batch_freq <- tryCatch({
+        open_dataset(glue("s3://{bucket}/maf/{jira}/")) %>%
+            collect() %>%
+            mutate(snp=manifest$names)
+    }, error = function(e) {
+        if (startsWith(e$message,'IOError: Path does not exist')) {
+            return(tibble())
+        }
+        stop(e$message)
+    })
+    if (nrow(batch_freq) == 0) {
+        # else calculate them
+        batch_freq <- sample.info %>% 
+            filter(JIRA==jira) %>%
+            group_by(Sample_ID) %>%
+            collect() %>%
+            summarize(get_genotypes(Barcode,Position,bucket), .groups="drop") %>%
+            group_by(snp, abgeno) %>%
+            summarize(n=n(), .groups="drop_last") %>%
+            spread(abgeno, n) %>%
+            replace(is.na(.), 0) %>%
+            summarize(maf=(AB+2*BB)/(2*(AA+AB+BB)))
 
-    # Cache the batch frequencies for this project/batch
-    batch.freq %>% select(maf) %>% as.data.table() %>% fwrite(filename)
-    s3$put_object(Body=filename,
-                  Bucket=bucket,
-                  Key=key)
-    
-    return(batch.freq)
+        # Cache the batch frequencies for this project/batch
+        batch_freq %>% select(maf) %>%
+            write_dataset(glue("s3://{bucket}/maf/{jira}/"))
+    }
+    return(batch_freq)
 }
 
-get.allele.data <- function(sample, manifest) {
-    key <- glue::glue('{sample$Barcode}_{sample$Position}.gtc')
-    filename <- paste0("/tmp/",key)
-    if (!file.exists(filename)) {
-        s3$download_file(bucket, key, filename)
-    }
-    gtc <- bead.array.files$GenotypeCalls(filename)
-    return(unlist(map(gtc$get_genotypes(),
-            ~ bead.array.files$code2genotype[.+1])))
+# Get genotype data for each sample in the sample list
+get_genotypes <- function(Barcode, Position, bucket) {
+    gtc <- bead_array_files$GenotypeCalls(glue("s3://{bucket}/{Barcode}/{Position}.gtc"))
+    abgeno <- gtc$get_genotypes()
+    tibble(snp=manifest$names,abgeno=abgeno) %>%
+        mutate(abgeno=bead_array_files$code2genotype[abgeno+1])
 }
