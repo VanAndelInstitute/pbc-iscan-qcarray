ff051424cbb6a4dd6f84dccb5cd64c9dba94c66a
EC2 Default User <ec2-user@ip-172-31-14-158.us-east-2.compute.internal>
2022-06-10 01:11:44 +0000

    clean up sample_info.parquet dataset

--- snakemake/sample_info.py
+++ snakemake/sample_info.py
@@ -20,7 +20,7 @@ convert_pairing = lambda x: "tumor-normal" if "2" in str(x) else "tumor-normal-n
 colnames = ['BSI_ID', 'Subject_ID', 'Subject ID', 'Anatomic_Site', 'Anatomic Site', 'Sample_Type', 'Sample Type', 'Within_batch_pairing', 'Within batch pairing']
 manifest = wr.s3.read_excel(f's3://{manifest}',
                     usecols=lambda x: x in colnames,
-                    converters={'Within_batch_pairing':convert_pairing })
+                    converters={'Within_batch_pairing': convert_pairing, 'Within batch pairing': convert_pairing})
 colname_map = {'BSI_ID': 'Sample_ID', 'Subject ID': 'Subject_ID', 'Anatomic Site': 'Anatomic_Site', 'Sample Type': 'Sample_Type', 'Within batch pairing': 'Within_batch_pairing'}
 manifest.rename(columns=colname_map, inplace=True)
 
@@ -35,8 +35,8 @@ exp_pairs_list = exp_pairs.groupby('Sample_ID').agg(list)
 
 # Combine files
 merged = manifest.merge(sample_sheet, how='left', on='Sample_ID').merge(exp_pairs_list, how='left', on='Sample_ID')
-df = merged[merged.Sample_ID != "EMTPY"]
+df = merged[~merged.Sample_ID.isin(["EMPTY", "EMTPY","empty"]) & ~merged.Sample_ID.isnull()]
 
 # Write to Parquet column store
-table = pa.Table.from_pandas(df)
+table = pa.Table.from_pandas(df, preserve_index=False)
 pq.write_table(table, output_file)
